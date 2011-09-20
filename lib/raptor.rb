79b888644b0d6df8e572bcb418f9581ef54f8245
Tom Crayford <tcrayford@gmail.com>
2011-09-20 11:32:23 -0700

    Can now dispatch routes by their url.

--- lib/raptor.rb
+++ lib/raptor.rb
@@ -2,12 +2,41 @@ require 'erb'
 
 module Raptor
   def self.routes(resource)
-    Router.new(resource)
+    Router.new(resource, [
+               Route.new('/posts/new', 'Posts::Record#new', 'new', resource),
+               Route.new('/posts/:id', 'Posts::Record#find_by_id', 'show', resource)
+
+    ])
   end
 
   class Router
-    def initialize(resource)
+    def initialize(resource, routes)
       @resource = resource
+      @routes = routes
+    end
+
+    def call(env)
+      incoming_path = env['PATH_INFO']
+      route = @routes.find {|r| r.matches?(incoming_path) }
+      route.call(env)
+    end
+  end
+
+  class Route
+    attr_reader :template_name
+    def initialize(path, domain_spec, template_name, resource)
+      @path = path
+      @domain_spec = domain_spec
+      @template_name = template_name
+      @resource = resource
+    end
+
+    def call(env)
+      incoming_path = env['PATH_INFO']
+      args = args_from_incoming_path(incoming_path)
+      record = record_class.send(domain_method(@domain_spec), *args)
+      presenter = one_presenter.new(record)
+      render(presenter, template_name)
     end
 
     def resource_name
@@ -18,21 +47,39 @@ module Raptor
       string.gsub(/(.)([A-Z])/, '\1_\2').downcase
     end
 
-    def call(env)
-      id = extract_id(env['PATH_INFO'])
-      record = record_class.find_by_id(id)
-      template = ERB.new(File.new("views/#{resource_name}/show.html.erb").read)
-      presenter = one_presenter.new(record)
+
+    def matches?(path)
+      zip_with_path(path).map do |route_component, path_component|
+        (route_component[0] == ':' || route_component == path_component)
+      end.all?
+    end
+
+    def args_from_incoming_path(path)
+      zip_with_path(path).select do |route_component, path_component|
+        route_component[0] == ':'
+      end.map {|x| x[1].to_i } # all url args are numbers for now
+    end
+
+    def zip_with_path(path)
+      @path.split('/').zip(path.split('/'))
+    end
+
+    def render(presenter, template_name)
+      template = template_named(template_name)
       template_binder = TemplateBinder.new(resource_name.to_sym => presenter)
       template.result(template_binder.get_binding)
+    end
+
+    def template_named(route)
+      template = ERB.new(File.new("views/#{resource_name}/#{route}.html.erb").read)
     end
 
     def record_class
       @resource.const_get(:Record)
     end
 
-    def extract_id(path)
-      path.split('/').last.to_i
+    def domain_method(domain_description)
+      domain_description.split('#').last.to_sym
     end
 
     def one_presenter
--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -9,6 +9,7 @@ module FakeResources::Post
 
   Routes = Raptor.routes(self) do
     show 'Posts::Record#find_by_id'
+    new 'Posts::Record#new'
   end
 
   class PresentsOne
@@ -35,15 +36,45 @@ end
 
 describe "Router" do
   it "routes requests through the record, presenter, and template" do
-    env = {'PATH_INFO' => 'posts/5'}
+    env = {'PATH_INFO' => '/posts/5'}
     rendered = FakeResources::Post::Routes.call(env)
     rendered.strip.should == "It's FIRST POST!"
   end
 
   it "knows the name of resources with camel cased names" do
-    Raptor::Router.new(stub(:name => 'CamelCase')).resource_name.should == 'camel_case'
+    Raptor::Route.new(stub, stub, stub, stub(:name => 'CamelCase')).resource_name.should == 'camel_case'
+  end
+
+  it "routes requests to new" do
+    env = {'PATH_INFO' => '/posts/new'}
+    rendered = FakeResources::Post::Routes.call(env)
+    rendered.strip.should == "<form>\n</form>"
   end
 
   it "raises an error when templates access undefined variables"
+end
+
+describe "Route matching" do
+  it "matches if the paths are the same" do
+    Raptor::Route.new('/posts/new', stub, stub, stub).matches?('/posts/new').should be_true
+  end
+
+  it "does not match when the paths are different" do
+    Raptor::Route.new('/posts/new', stub, stub, stub).matches?('/foo/bar').should be_false
+  end
+
+  it "matches any component when a path has a param" do
+    Raptor::Route.new('/posts/:id', stub, stub, stub).matches?('/posts/5').should be_true
+  end
+
+  it "does not a path with params when the path does not match" do
+    Raptor::Route.new('/posts/:id', stub, stub, stub).matches?('/users/2').should be_false
+  end
+end
+
+describe "pulling the args out of a route spec and an incoming path" do
+  it "pulls out args that match with keywords" do
+    Raptor::Route.new('/posts/:id', stub, stub, stub).args_from_incoming_path('/posts/5').should == [5]
+  end
 end
 
