67c822dcbfc08a251ba885f41262ea3abd4f1b59
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-23 21:45:40 -0700

    move FakeResources to its own file

---
+++ spec/fake_resources.rb
@@ -0,0 +1,34 @@
+module FakeResources; end
+module FakeResources::Post
+  # A resource with:
+  #   - One record, ID 5, whose title is "first post"
+  #   - A presenter that upcases records' titles
+  #   - A template that says "It's #{post.title}!"
+
+  Routes = Raptor.routes(self) do
+    new 'Posts::Record#new'
+    show 'Posts::Record#find_by_id'
+  end
+
+  class PresentsOne
+    def initialize(post)
+      @post = post
+    end
+
+    def title
+      @post.title.upcase
+    end
+  end
+
+  class Record
+    def title
+      "first post"
+    end
+
+    def self.find_by_id(id)
+      records = {5 => Record.new}
+      records.fetch(id)
+    end
+  end
+end
+
