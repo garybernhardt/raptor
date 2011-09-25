bbbbaaa2b66ed4af49b9bc825225c106ecbc8d15
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-25 15:26:31 -0700

    eliminate duplication: stubs

--- spec/infers_args_spec.rb
+++ spec/infers_args_spec.rb
@@ -7,27 +7,30 @@ describe Raptor::InfersArgs do
   def method_taking_splat(*); end
   def method_taking_nothing; end
 
+  before do
+    @request = stub(:path_info => '/post/5', :params => stub)
+  end
+
   it "infers required arguments for delegate methods" do
-    Raptor::InfersArgs.for(stub(:path_info => 'post/5', :params => stub),
+    Raptor::InfersArgs.for(@request,
                            method(:method_taking_id),
                            stub(:extract_args => {:id => 5})).must_equal [5]
   end
 
   it "infers :params" do
-    params = {'params' => 'hash'}
-    Raptor::InfersArgs.for(stub(:path_info => 'post/new', :params => params),
+    Raptor::InfersArgs.for(@request,
                            method(:method_taking_params),
-                           stub(:extract_args => {})).must_equal [params]
+                           stub(:extract_args => {})).must_equal [@request.params]
   end
 
   it "infers [] when the method only takes optional parameters" do
-    Raptor::InfersArgs.for(stub,
+    Raptor::InfersArgs.for(@request,
                            method(:method_taking_splat),
                            stub).must_equal []
   end
 
   it "infers [] when the method takes nothing" do
-    Raptor::InfersArgs.for(stub,
+    Raptor::InfersArgs.for(@request,
                            method(:method_taking_nothing),
                            stub).must_equal []
   end
