88a1430f92e681ac5402f1e6d1bcdc038af92d4c
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-25 15:21:58 -0700

    replace method stubs with real methods

--- spec/infers_args_spec.rb
+++ spec/infers_args_spec.rb
@@ -2,24 +2,27 @@ require './spec/spec_helper'
 require 'mocha'
 
 describe Raptor::InfersArgs do
+  def method_taking_id(id); end
+  def method_taking_params(params); end
+  def method_taking_splat(*); end
+
   it "infers required arguments for delegate methods" do
     Raptor::InfersArgs.for(stub(:path_info => 'post/5', :params => stub),
-                           stub(:name => :a_method, :parameters => [[:req, :id]]),
+                           method(:method_taking_id),
                            stub(:extract_args => {:id => 5})).must_equal [5]
   end
 
   it "infers :params" do
     params = {'params' => 'hash'}
     Raptor::InfersArgs.for(stub(:path_info => 'post/new', :params => params),
-                           stub(:name => :a_method, :parameters => [[:req, :params]]),
+                           method(:method_taking_params),
                            stub(:extract_args => {})).must_equal [params]
   end
 
   it "infers [] when the method only takes optional parameters" do
     Raptor::InfersArgs.for(stub,
-                           stub(:name => :a_method, :parameters => [[:rest]]),
+                           method(:method_taking_splat),
                            stub).must_equal []
   end
 end
-
 
