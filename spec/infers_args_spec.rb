1f96353559f70c062b84a7fe925f1c07f87ccc61
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-25 15:28:53 -0700

    add infer helper method in specs

--- spec/infers_args_spec.rb
+++ spec/infers_args_spec.rb
@@ -9,30 +9,27 @@ describe Raptor::InfersArgs do
 
   before do
     @request = stub(:path_info => '/post/5', :params => stub)
+    @path = stub(:extract_args => {:id => 5})
+  end
+
+  def infer(method_name)
+    Raptor::InfersArgs.for(@request, method(method_name), @path)
   end
 
   it "infers required arguments for delegate methods" do
-    Raptor::InfersArgs.for(@request,
-                           method(:method_taking_id),
-                           stub(:extract_args => {:id => 5})).must_equal [5]
+    infer(:method_taking_id).must_equal [5]
   end
 
   it "infers :params" do
-    Raptor::InfersArgs.for(@request,
-                           method(:method_taking_params),
-                           stub(:extract_args => {})).must_equal [@request.params]
+    infer(:method_taking_params).must_equal [@request.params]
   end
 
   it "infers [] when the method only takes optional parameters" do
-    Raptor::InfersArgs.for(@request,
-                           method(:method_taking_splat),
-                           stub).must_equal []
+    infer(:method_taking_splat).must_equal []
   end
 
   it "infers [] when the method takes nothing" do
-    Raptor::InfersArgs.for(@request,
-                           method(:method_taking_nothing),
-                           stub).must_equal []
+    infer(:method_taking_nothing).must_equal []
   end
 end
 
