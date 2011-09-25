bb1ae49aa7594b10931148410ad124af55d236b0
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-24 19:57:41 -0700

    switch specs to minitest

--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -1,23 +1,27 @@
+require 'minitest/autorun'
+require 'mocha'
+
+$LOAD_PATH << "lib" << "spec"
 require 'raptor'
 require 'fake_resources'
 
 describe Raptor::App do
-  let(:app) do
+  def app
     Raptor::App.new([FakeResources::Post, FakeResources::WithNoBehavior])
   end
 
   it "routes to multiple resources" do
     env = {'PATH_INFO' => '/post/5'}
-    app.call(env).strip.should == "It's FIRST POST!"
+    app.call(env).strip.must_equal "It's FIRST POST!"
     env = {'PATH_INFO' => '/with_no_behavior/5'}
-    app.call(env).strip.should == "The index!"
+    app.call(env).strip.must_equal "The index!"
   end
 
   it "raises an error if no route matches" do
     env = {'PATH_INFO' => '/resource_that_doesnt_exist/5'}
-    expect do
+    Proc.new do
       app.call(env)
-    end.to raise_error(Raptor::NoRouteMatches)
+    end.must_raise(Raptor::NoRouteMatches)
   end
 end
 
@@ -25,62 +29,62 @@ describe Raptor::Router do
   it "routes requests through the record, presenter, and template" do
     env = {'PATH_INFO' => '/post/5'}
     rendered = FakeResources::Post::Routes.call(env)
-    rendered.strip.should == "It's FIRST POST!"
+    rendered.strip.must_equal "It's FIRST POST!"
   end
 
   it "routes requests to new" do
     env = {'PATH_INFO' => '/post/new'}
     rendered = FakeResources::Post::Routes.call(env)
-    rendered.strip.should == "<form>\n</form>"
+    rendered.strip.must_equal "<form>\n</form>"
   end
 
-  context "when a route isn't defined" do
+  describe "when a route isn't defined" do
     it "raises an error" do
       env = {'PATH_INFO' => '/doesnt_exist'}
-      expect do
+      Proc.new do
         FakeResources::Post::Routes.call(env)
-      end.to raise_error(Raptor::NoRouteMatches)
+      end.must_raise(Raptor::NoRouteMatches)
     end
   end
 
   it "has an index" do
     env = {'PATH_INFO' => '/with_no_behavior/index'}
-    FakeResources::WithNoBehavior::Routes.call(env).strip.should == "The index!"
+    FakeResources::WithNoBehavior::Routes.call(env).strip.must_equal "The index!"
   end
 
   it "raises an error when templates access undefined methods" do
     env = {'PATH_INFO' => '/with_undefined_method_call_in_index/index'}
-    expect do
+    Proc.new do
       FakeResources::WithUndefinedMethodCallInIndex::Routes.call(env)
-    end.to raise_error(NameError,
-                       /undefined local variable or method `undefined_method'/)
+    end.must_raise(NameError,
+                   /undefined local variable or method `undefined_method'/)
   end
 
   it "knows when a route matches" do
-    FakeResources::Post::Routes.matches?("/post/new").should be_true
+    FakeResources::Post::Routes.matches?("/post/new").must_equal true
   end
 
   it "knows when routes don't match" do
-    FakeResources::Post::Routes.matches?("/not_a_post/new").should be_false
+    FakeResources::Post::Routes.matches?("/not_a_post/new").must_equal false
   end
 end
 
 describe Raptor::RoutePath do
-  context "route matching" do
+  describe "route matching" do
     it "matches if the paths are the same" do
-      matches?('/post/new', '/post/new').should be_true
+      matches?('/post/new', '/post/new').must_equal true
     end
 
     it "does not match when the paths are different" do
-      matches?('/post/new', '/foo/bar').should be_false
+      matches?('/post/new', '/foo/bar').must_equal false
     end
 
     it "matches any component when a path has a param" do
-      matches?('/post/:id', '/post/5').should be_true
+      matches?('/post/:id', '/post/5').must_equal true
     end
 
     it "does not a path with params when the path does not match" do
-      matches?('/post/:id', '/users/2').should be_false
+      matches?('/post/:id', '/users/2').must_equal false
     end
 
     def matches?(route, url)
@@ -88,27 +92,27 @@ describe Raptor::RoutePath do
     end
   end
 
-  context "pulling the args out of a route spec and an incoming path" do
+  describe "pulling the args out of a route spec and an incoming path" do
     it "pulls out args that match with keywords" do
-      Raptor::RoutePath.new('/post/:id').extract_args('/post/5').should == [5]
+      Raptor::RoutePath.new('/post/:id').extract_args('/post/5').must_equal [5]
     end
   end
 end
 
 describe Raptor::Resource do
-  let(:camel_case_resource) { Raptor::Resource.new(stub(:name => 'CamelCase')) }
-  let(:resource) { Raptor::Resource.new(FakeResources::Post) }
+  def camel_case_resource; Raptor::Resource.new(stub(:name => 'CamelCase')); end
+  def resource; Raptor::Resource.new(FakeResources::Post); end
 
   it "knows the name of resources with camel cased names" do
-    camel_case_resource.resource_name.should == 'camel_case'
+    camel_case_resource.resource_name.must_equal 'camel_case'
   end
 
   it "knows how to get the record class" do
-    resource.record_class.should == FakeResources::Post::Record
+    resource.record_class.must_equal FakeResources::Post::Record
   end
 
   it "knows how to get the presenter" do
-    resource.one_presenter.should == FakeResources::Post::PresentsOne
+    resource.one_presenter.must_equal FakeResources::Post::PresentsOne
   end
 end
 
