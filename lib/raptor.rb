f20a9464c17ec118e97d6636897bf81ebac03571
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-28 15:29:14 -0700

    extract variable

--- lib/raptor.rb
+++ lib/raptor.rb
@@ -7,9 +7,10 @@ class Class
   end
 
   def define_initialize(args)
+    assignments = args.map { |a| "@#{a} = #{a}" }.join("\n")
     self.class_eval %{
       def initialize(#{args.join(", ")})
-        #{args.map { |a| "@#{a} = #{a}" }.join("\n")}
+        #{assignments}
       end
     }
   end
