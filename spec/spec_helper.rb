8f17eadd920e54f64d7eb361936ca306cd977d2c
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-29 15:08:00 -0700

    remove rack instantiation penalty for first test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,8 @@ def request(method, path, body="")
   Rack::Request.new(env(method, path, body))
 end
 
+# The first instantiation of these is very slow for some reason. Do it here so
+# it doesn't pollute test runtimes.
+Rack::Request.new({})
+Rack::Response.new
+
