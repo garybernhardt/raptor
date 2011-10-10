aa6dd5f4c281e08a7845e818487516a82d22fbe7
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-10-09 17:22:19 -0700

    indentation

--- spec/route_validation_spec.rb
+++ spec/route_validation_spec.rb
@@ -4,28 +4,26 @@ require_relative "../lib/raptor/validation"
 
 describe "route validation" do
   context "the route params" do
+    it "rejects routes with both a render and a redirect" do
+      redirect_and_render = {:redirect => :index, :render => :show}
+      expect do
+        Raptor::ValidatesRoutes.validate_route_params!(redirect_and_render)
+      end.to raise_error(Raptor::ConflictingRoutes)
+    end
 
-  it "rejects routes with both a render and a redirect" do
-    redirect_and_render = {:redirect => :index, :render => :show}
-    expect do
-      Raptor::ValidatesRoutes.validate_route_params!(redirect_and_render)
-    end.to raise_error(Raptor::ConflictingRoutes)
-  end
+    it "does not reject route params with just a redirect" do
+      just_redirect = {:redirect => :index}
+      Raptor::ValidatesRoutes.validate_route_params!(just_redirect)
+    end
 
-  it "does not reject route params with just a redirect" do
-    just_redirect = {:redirect => :index}
-    Raptor::ValidatesRoutes.validate_route_params!(just_redirect)
-  end
+    it "does not reject route params with just a render" do
+      just_render = {:render => :index}
+      Raptor::ValidatesRoutes.validate_route_params!(just_render)
+    end
 
-  it "does not reject route params with just a render" do
-    just_render = {:render => :index}
-    Raptor::ValidatesRoutes.validate_route_params!(just_render)
-  end
-
-  it "does not reject empty route params" do
-    empty_params = {}
-    Raptor::ValidatesRoutes.validate_route_params!(empty_params)
-  end
-
+    it "does not reject empty route params" do
+      empty_params = {}
+      Raptor::ValidatesRoutes.validate_route_params!(empty_params)
+    end
   end
 end
