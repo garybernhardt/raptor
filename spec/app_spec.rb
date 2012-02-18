e0a2609251e354b2a5e136eed94993005f088754
Tom Crayford <tcrayford@googlemail.com>
2012-02-18 00:13:22 +0000

    add tests for method override

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -18,6 +18,24 @@ describe Raptor::App do
 
   let(:app) { Raptor::App.new(App) }
 
+  describe 'method override' do
+    def request_method(method)
+      hash_including('REQUEST_METHOD' => method)
+    end
+
+    it "tunnels PUT over POST" do
+      App::Routes.should_receive(:call).with(request_method('PUT'))
+      Raptor::App.new(App).call(env('POST', '/irrelevant',
+                                            StringIO.new('_method=PUT')))
+    end
+
+    it "tunnels DELETE over POST" do
+      App::Routes.should_receive(:call).with(request_method('DELETE'))
+      Raptor::App.new(App).call(env('POST', '/irrelevant',
+                                               StringIO.new('_method=DELETE')))
+    end
+  end
+
   it "routes to resources" do
     File.stub(:new).with("views/post/index.html.erb").
       and_return(stub(:read => "Template content"))
